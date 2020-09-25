64e512716aae96e2d5218c01b760a4a2be0ccea3
Mészár Máté <mmate199909@gmail.com>
2020-09-25 11:38:43 +0200

    Adding save option.

--- UserMaintenance/UserMaintenance/Form1.Designer.cs
+++ UserMaintenance/UserMaintenance/Form1.Designer.cs
@@ -32,6 +32,7 @@
             this.lblLastName = new System.Windows.Forms.Label();
             this.txtLastName = new System.Windows.Forms.TextBox();
             this.btnAdd = new System.Windows.Forms.Button();
+            this.writeToFile = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // listUsers
@@ -68,11 +69,22 @@
             this.btnAdd.UseVisualStyleBackColor = true;
             this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
             // 
+            // writeToFile
+            // 
+            this.writeToFile.Location = new System.Drawing.Point(461, 138);
+            this.writeToFile.Name = "writeToFile";
+            this.writeToFile.Size = new System.Drawing.Size(169, 23);
+            this.writeToFile.TabIndex = 6;
+            this.writeToFile.Text = "button1";
+            this.writeToFile.UseVisualStyleBackColor = true;
+            this.writeToFile.Click += new System.EventHandler(this.writeToFile_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.writeToFile);
             this.Controls.Add(this.btnAdd);
             this.Controls.Add(this.txtLastName);
             this.Controls.Add(this.lblLastName);
@@ -90,6 +102,7 @@
         private System.Windows.Forms.Label lblLastName;
         private System.Windows.Forms.TextBox txtLastName;
         private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button writeToFile;
     }
 }
 
--- UserMaintenance/UserMaintenance/Resource1.Designer.cs
+++ UserMaintenance/UserMaintenance/Resource1.Designer.cs
@@ -77,5 +77,14 @@ namespace UserMaintenance {
                 return ResourceManager.GetString("FullName", resourceCulture);
             }
         }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Fájlba írás.
+        /// </summary>
+        internal static string WriteToFile {
+            get {
+                return ResourceManager.GetString("WriteToFile", resourceCulture);
+            }
+        }
     }
 }
