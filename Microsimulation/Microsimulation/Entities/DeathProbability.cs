f177c5f3eb780aac9a743a2c1218e8e4fe2d6c4f
Mészár Máté <mmate199909@gmail.com>
2020-10-21 14:26:57 +0200

    Loading Data.

---
+++ Microsimulation/Microsimulation/Entities/DeathProbability.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Microsimulation.Entities
+{
+    public class DeathProbability
+    {
+        public Gender Gender { get; set; }
+        public int Age { get; set; }
+        public double Probability { get; set; }
+    }
+}
