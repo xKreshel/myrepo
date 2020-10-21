919d09187ac823b7a483ba7dd528e90ff88c5d69
Mészár Máté <mmate199909@gmail.com>
2020-10-21 15:48:13 +0200

    Creating random number.

--- Microsimulation/Microsimulation/Form1.cs
+++ Microsimulation/Microsimulation/Form1.cs
@@ -17,6 +17,7 @@ namespace Microsimulation
         List<Person> Population = new List<Person>();
         List<BirthProbability> BirthProbabilities = new List<BirthProbability>();
         List<DeathProbability> DeathProbabilities = new List<DeathProbability>();
+        Random rng = new Random(1234);
         public Form1()
         {
             InitializeComponent();
