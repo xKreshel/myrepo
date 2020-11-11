2f67273c842624cc11a837abf89310c5950eb225
Mészár Máté <mmate199909@gmail.com>
2020-11-11 10:32:50 +0100

    Creating CarFactory class.

--- Factory/Factory/Entities/CarFactory.cs
+++ Factory/Factory/Entities/CarFactory.cs
@@ -7,11 +7,11 @@ using System.Threading.Tasks;
 
 namespace Factory.Entities
 {
-    public class BallFactory : IToyFactory
+    public class CarFactory : IToyFactory
     {
         public Toy CreateNew()
         {
-            return new Ball();
+            return new Car();
         }
     }
 }
