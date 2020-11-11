a84ed51f42538b94bc93d7c337854ec930d15756
Mészár Máté <mmate199909@gmail.com>
2020-11-11 10:00:46 +0100

    Creating a conveyor

--- Factory/Factory/Entities/Ball.cs
+++ Factory/Factory/Entities/Ball.cs
@@ -8,7 +8,7 @@ using System.Windows.Forms;
 
 namespace Factory.Entities
 {
-    class Ball : Label
+    public class Ball : Label
     {
         public Ball()
         {
--- Factory/Factory/Entities/BallFactory.cs
+++ Factory/Factory/Entities/BallFactory.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Factory.Entities
 {
-    class BallFactory
+    public class BallFactory
     {
         public Ball CreateNew()
         {
