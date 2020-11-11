9efd752de3d6a4f73576c5a159bd880c27ba6b25
Mészár Máté <mmate199909@gmail.com>
2020-11-11 10:25:46 +0100

    Creating Car class.

---
+++ Factory/Factory/Entities/Car.cs
@@ -0,0 +1,19 @@
+﻿using Factory.Abstractions;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Factory.Entities
+{
+    class Car : Toy
+    {
+        protected override void DrawImage(Graphics g)
+        {
+            Image imageFile = Image.FromFile("Images/car.png");
+            g.DrawImage(imageFile, new Rectangle(0, 0, Width, Height));
+        }
+    }
+}
--- Factory/Factory/Form1.cs
+++ Factory/Factory/Form1.cs
@@ -40,7 +40,7 @@ namespace Factory
             var maxPosition = 0;
             foreach (var ball in _balls)
             {
-                ball.MoveBall();
+                ball.MoveToy();
                 if (ball.Left > maxPosition)
                     maxPosition = ball.Left;
             }
